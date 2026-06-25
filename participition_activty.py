b6de75b12f629d538213d09e6f5f7c01c58168ed
Hriday <hridayvermani12@gmail.com>
2026-06-25 14:11:22 -0400

    Add 6-sided die initialization and 10 roll loop

--- participition_activty.py
+++ participition_activty.py
@@ -10,3 +10,10 @@ class Die:
     def roll_die(self):
         """Return a random number between 1 and the number of sides."""
         return random.randint(1, self.sides)
+    
+    # 1. Make a 6-sided die and roll it 10 times 
+print("--- Rolling a 6-sided die 10 times ---")
+six_sided_die = Die() # Uses the default value of 6
+for _ in range(10):
+    print(six_sided_die.roll_die(), end=" ")
+print("\n") # New line for spacing
